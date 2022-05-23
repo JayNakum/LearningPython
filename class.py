93497bddef27f1c94c9a07ca9acc123f1a7ba00d
Jay Nakum <nakum.jay.m@gmail.com>
2022-05-23 21:10:34 +0530

    learning python

--- class.py
+++ class.py
@@ -1,5 +1,16 @@
-class Bruuuh:
-    def __init__(self) -> None:
-        print("2-4-6-8 Who do we appreciate?\nJava")
+# class Bruuuh:
+#     def __init__(self) -> None:
+#         print("2-4-6-8 Who do we appreciate?\nJava")
 
-Bruuuh()
+# Bruuuh()
+
+
+class B:
+    def test(self):
+        print(10)
+
+class C(B):
+    def test(self):
+        print(20)
+
+C().test()
---
+++ panda.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+dataframe = pd.DataFrame([1, 2, 3, 4])
+print(dataframe)
