64b1317ae4c636a64bb4d98c8551241d62f4a766
Jay Nakum <nakum.jay.m@gmail.com>
2022-08-17 12:26:11 +0530

    playfair done

--- Information Security/Lab2.py
+++ Information Security/Lab2.py
@@ -2,7 +2,6 @@
 
 def getKeyMatrix(key: str) -> list:
     keyMatrix = []
-
     ALPHA = 'abcdefghiklmnopqrstuvwxyz'
     keyList = []
     indexK = 0
@@ -10,11 +9,11 @@ def getKeyMatrix(key: str) -> list:
 
     for _ in range((len(ALPHA) + len(key))):
         if(indexK < len(key)):
-            if ((key[indexK].lower() not in keyList)):
+            if (key[indexK].lower() not in keyList):
                 keyList.append(key[indexK])
             indexK += 1
         else:
-            if ((ALPHA[indexA].lower() not in keyList)):
+            if (ALPHA[indexA] not in keyList):
                 keyList.append(ALPHA[indexA])
             indexA += 1
 
@@ -26,39 +25,99 @@ def getKeyMatrix(key: str) -> list:
         if (count % 5 == 0):
             keyMatrix.append(row)
             row = []
-
+    print(keyMatrix)
     return keyMatrix
+
+def getPosFromMatrix(char:str, matrix:list) -> tuple:
+    for row in range(5):
+        for col in range(5):
+            if(char == matrix[row][col]):
+                # print(row, col)
+                return (row, col)
+
+    return (-1, -1)
+
+
+def getPairs(message:str) -> list:
+    pairs = []
+    
+    i = 0
+    while (i < len(message)):
+        p1 = message[i]
+        i += 1
+
+        if (i == len(message)):
+            p2 = 'x'
+        else:
+            p2 = message[i]
+        
+        if (p1 == p2):
+            p2 = 'x'
+        else:
+            i += 1
+
+        pair = p1 + p2
+        pairs.append(pair)
+
+    print(pairs)
+    return pairs
 
 def encrypt(key:str, message:str) -> str:
     cipher = ''
     keyMatrix = getKeyMatrix(key)
-    plainText = []
-
-
-    for i in range(0, len(message), 2):
-        
-        pair = message[i]
-        if (message[i] == message[i + 1]):
-            pair += 'x'
-            i -= 1
-        else:
-            pair += message[i + 1]
-        
-        plainText.append(pair)
-        # i += 1
-            
-
-
-    print(plainText)
+    pairs = getPairs(message)
     
+    for pair in pairs:        
+        posP = getPosFromMatrix(pair[0], keyMatrix)
+        posQ = getPosFromMatrix(pair[1], keyMatrix)
+
+        if (posP[0] == posQ[0]):
+            cipher += keyMatrix[posP[0]][(posP[1] + 1) % 5]
+            cipher += keyMatrix[posQ[0]][(posQ[1] + 1) % 5]
+        elif (posP[1] == posQ[1]):
+            cipher += keyMatrix[(posP[0] + 1) % 5][posP[1]]
+            cipher += keyMatrix[(posQ[0] + 1) % 5][posQ[1]]
+        else:
+            cipher += keyMatrix[posP[0]][posQ[1]]
+            cipher += keyMatrix[posQ[0]][posP[1]]
+
+    return cipher
+
+def decrypt(key:str, cipher:str) -> str:
+    message = ''
+    keyMatrix = getKeyMatrix(key)
+    pairs = getPairs(cipher)
+    
+    for pair in pairs:        
+        posP = getPosFromMatrix(pair[0], keyMatrix)
+        posQ = getPosFromMatrix(pair[1], keyMatrix)
+
+        if (posP[0] == posQ[0]):
+            message += keyMatrix[posP[0]][(posP[1] - 1) % 5]
+            message += keyMatrix[posQ[0]][(posQ[1] - 1) % 5]
+        elif (posP[1] == posQ[1]):
+            message += keyMatrix[(posP[0] - 1) % 5][posP[1]]
+            message += keyMatrix[(posQ[0] - 1) % 5][posQ[1]]
+        else:
+            message += keyMatrix[posP[0]][posQ[1]]
+            message += keyMatrix[posQ[0]][posP[1]] 
+
+    return message
 
 
 if __name__ == "__main__":
     # message = input('Enter your message: ')
     # key = int(input('Enter key: '))
-    message = 'moonmission'
-    key = 'Engineering'
+    key = 'occurence'
+    # o c u r e
+    # n a b d f
+    # g h i k l
+    # m p q s t
+    # v w x y z
+    message = 'talltrees'
 
-    # print(getKeyMatrix(key))
-    encrypt(key, message)
+    cipher = encrypt(key, message)
+    print(cipher)
+    plaintext = decrypt(key, cipher)
+    print(plaintext)
     
